ca8268b3c578d3c702f91afb84c90e34f751cc81
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-10 08:05:46 -0300

    Create ToInfinitySpeed.js

---
+++ OldErrorUI/ToInfinitySpeed.js
@@ -0,0 +1,57 @@
+import { Popup } from "../api/ui/Popup";
+import { Color } from "../api/ui/properties/Color";
+import { ImageSource } from "../api/ui/properties/ImageSource";
+import { Thickness } from "../api/ui/properties/Thickness";
+import { ui } from "../api/ui/UI"
+
+var id = "?";
+var name = "To Infinity! on UI : Acceleration Button";
+var description = "huh";
+var authors = "Skyhigh173#3120";
+var version = "v1";
+
+var value = 1, currency;
+var accPress = false;
+var TickPress = 0;
+
+var init = () => {
+    currency = theory.createCurrency();
+}
+
+var getEquationOverlay = () => {
+    let stack = ui.createStackLayout({
+        children: [
+            ui.createImage({
+                source: ImageSource.ACCELERATE,
+                verticalOptions: LayoutOptions.END,
+                horizontalOptions: LayoutOptions.START,
+                heightRequest: 42, // universe number : 42
+                onTouched: (e) => {
+                    if(e.type == TouchType.PRESSED) accPress = true;
+                    else if(e.type.isReleased()) accPress = false;
+                }
+            }),
+            ui.createLabel({text: () => Math.round(value * 100) / 100 + "x"})
+        ]
+    })
+    return stack;
+}
+var tick = (elapsedTime, multiplier) => {
+    let dt = BigNumber.from(elapsedTime * multiplier);
+    let bonus = theory.publicationMultiplier;
+    if (accPress) TickPress += 1e300 * dt;
+    if (!accPress) TickPress -= 4 * dt;
+    if (TickPress <= 0) TickPress = 0;
+    value = 1;
+    //if (accPress)
+    value *= Math.pow((9 * TickPress + 1), 1 / 9);
+    //else
+    //    value /= Math.pow(100/TickPress, 3);
+    
+    currency.value += value;
+    theory.invalidatePrimaryEquation();
+}
+var getPrimaryEquation = () => {
+    return "\\dot{\\rho} = " + Math.round(value * 100) / 100 + " \\ " + Math.round(TickPress);
+}
+init();
